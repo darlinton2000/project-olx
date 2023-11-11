aaab7f3b6c337f73d92d1be36635b85bed049026
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-11-11 10:58:31 -0300

    Implementado funcao tipada

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\JsonResponse;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/ping', function (): JsonResponse {
+    return response()->json(['Pong' => true]);
 });
