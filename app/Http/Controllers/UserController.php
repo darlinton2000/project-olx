5db31b41dfb450f8a440c46ccd1c7bfc0ab4303e
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-11-11 13:19:50 -0300

    Segurança de senhas e registro

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,17 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreateUserRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    /**
+     * Register user in database
+     *
+     * @param CreateUserRequest $request
+     * @return JsonResponse
+     */
     public function signup(CreateUserRequest $request): JsonResponse
     {
         $data = $request->only(['name', 'email', 'password', 'state_id']);
+        $data['password'] = Hash::make($data['password']);
         $user = User::create($data);
 
         $response = [
             'error' => '',
-            'user' => $user
+            'token' => $user->createToken('Register_token')->plainTextToken
         ];
 
         return response()->json($response);
